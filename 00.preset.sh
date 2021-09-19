b7b8905e7e7ccd0c37f1afa6f053c21200bf5ac2
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-09-19 19:58:05 -0400

    001.003

--- 00.preset.sh
+++ 00.preset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #titul---------------+
 #
-#ver.001.002
+#ver.001.003
 #
 #--------------------+
 #
@@ -10,7 +10,7 @@ whonixcheck
 #update-------------------------------------------------------------------------------------------------------------+
 sudo apt-get update; sudo apt-get upgrade -y; sudo apt-get dist-upgrade -y
 #packeges-----------------------------------------------------------------------------------------------------------+
-sudo apt install autoconf automake bleachbit build-essential build-essential catfish dconf-cli dh-autoreconf engrampa etherape flameshot fonts-droid-fallback fonts-hack-ttf git git glib-2.0-dev gobject-introspection htop htop intltool libgtk2.0-dev libreoffice libreoffice-l10n-en-gb libxfce4ui-1-dev make mc nmap nmap npm onionshare openssh-client openssh-client openssl openssl openvpn openvpn pidgin pidgin-otr pkg-config psi-plus psi-plus python-exconsole qtox rdesktop sakura telegram-desktop terminator thunderbird tilix xfce4-goodies zsh-autosuggestions zsh-syntax-highlighting -y
+sudo apt install autoconf automake bleachbit build-essential build-essential catfish dconf-cli dh-autoreconf engrampa etherape flameshot fonts-droid-fallback fonts-hack-ttf git glib-2.0-dev gobject-introspection htop htop intltool libgtk2.0-dev libreoffice libreoffice-l10n-en-gb libxfce4ui-1-dev make mc nmap nmap npm onionshare openssh-client openssh-client openssl openssl openvpn openvpn pidgin pidgin-otr pkg-config psi-plus psi-plus python-exconsole qtox rdesktop sakura telegram-desktop terminator thunderbird tilix xfce4-goodies zsh-autosuggestions zsh-syntax-highlighting -y
 #update-torbrowser--------------------------------------------------------------------------------------------------+
 update-torbrowser --ordinary --noask
 #grub---------------------------------------------------------------------------------------------------------------+
@@ -19,12 +19,12 @@ sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo cp /etc/rads.d/30_default.conf /etc/rads.d/30_default.conf.bak
 sudo sed -i '52 s/rads_wait_seconds=10/rads_wait_seconds=3/g' /etc/rads.d/30_default.conf
 #git-vol------------------------------------------------------------------------------------------------------------+
-cd ~/; git clone https://github.com/supersimetria/lunar_whonix.git
-sh ~/lunar_whonix/01.useradd.sh
-sh ~/lunar_whonix/02.grub_timeout.sh
-sh ~/lunar_whonix/03.hand-assembled.sh
-sh ~/lunar_whonix/04.dotfiles.sh
-clear; sh ~/lunar_whonix/98.get_version_script.sh
+cd ~/; git clone https://github.com/supersimetria/L_whonix.git
+sh ~/lunar_whonix/01.compilation.sh
+sh ~/lunar_whonix/02.useradd.sh
+sh ~/lunar_whonix/03.dot.sh
+sudo rm -r L_whonix
 #link-file----------------------------------------------------------------------------------------------------------+
 #link https://git.io/JuC8u
 #link https://wwh-club.us/index.php?threads/statja-organizacija-raboty-na-os-whonix.72494/
+
