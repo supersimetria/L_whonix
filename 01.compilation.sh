f23b4a538111f71421904415f985ee770f989d38
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-09-20 19:54:10 -0400

    001.004

--- 01.compilation.sh
+++ 01.compilation.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #titul---------------+
 #
-#ver.001.003
+#ver.001.004
 #
 #--------------------+
 #
 #paper-icon-theme---------------------------------------------------------------------------------------------------+
-wget -O ~/paper-src.tar.gz https://github.com/snwh/paper-icon-theme/archive/refs/tags/v.1.5.0.tar.gz
-sudo mkdir /opt/paper-icon/; sudo tar -zxf paper-src.tar.gz -C /opt/paper-icon --strip-components 1
-cd /opt/paper-icon/; sudo bash autogen.sh
-sudo make
-sudo make install
-cd; sudo rm -r paper-src.tar.gz
+cd; git clone https://github.com/snwh/paper-icon-theme.git
+cd paper-icon-theme
+meson "build" --prefix=/usr
+sudo ninja -C "build" install
+cd ..; sudo rm -r paper-icon-theme
+#chicago95----------------------------------------------------------------------------------------------------------+
+cd; git clone https://github.com/grassmunk/Chicago95.git
+sudo cp -r Chicago95/Theme/Chicago95 /usr/share/themes/
+sudo cp -r Chicago95/Icons/* /usr/share/icons/
+sudo rm -r Chicago95/
 #infinalit----------------------------------------------------------------------------------------------------------+
 wget -O ~/infinality.deb https://launchpad.net/~no1wantdthisname/+archive/ubuntu/ppa/+files/fontconfig-infinality_20130104-0ubuntu0ppa1_all.deb
 sudo dpkg -i ~/infinality.deb; rm ~/infinality.deb
