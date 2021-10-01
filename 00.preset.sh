54deca4c4185257ebbffd2406b425f643d95f478
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-10-01 15:04:56 -0400

    001.016

--- 00.preset.sh
+++ 00.preset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #titul---------------+
 #
-#ver.001.015
+#ver.001.016
 #
 #--------------------+
 #
@@ -12,11 +12,6 @@ sudo -u sdwdate touch /run/sdwdate/first_success
 whonixcheck
 #packeges-----------------------------------------------------------------------------------------------------------+
 sudo apt install $(curl https://raw.githubusercontent.com/supersimetria/L_whonix/main/mod/01.1.pl-0) -y
-#remove-------------------------------------------------------------------------------------------------------------+
-sudo apt remove lightdm-autologin-greeter
-sudo apt remove --auto-remove
-#pip----------------------------------------------------------------------------------------------------------------+
-pip3 install b0mb3r
 #update-torbrowser--------------------------------------------------------------------------------------------------+
 update-torbrowser --ordinary --noask
 #grub---------------------------------------------------------------------------------------------------------------+
@@ -24,12 +19,6 @@ sudo sed -i '7 s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo cp /etc/rads.d/30_default.conf /etc/rads.d/30_default.conf.bak
 sudo sed -i '52 s/rads_wait_seconds=10/rads_wait_seconds=3/g' /etc/rads.d/30_default.conf
-#git-vol------------------------------------------------------------------------------------------------------------+
-cd ~/; git clone https://github.com/supersimetria/L_whonix.git
-#sh ~/L_whonix/01.compilation.sh
-#sh ~/L_whonix/02.useradd.sh
-#sh ~/L_whonix/03.dot.sh
-#sudo rm -r L_whonix
 #link-file----------------------------------------------------------------------------------------------------------+
 #link https://git.io/JuC8u
 #link https://wwh-club.us/index.php?threads/statja-organizacija-raboty-na-os-whonix.72494/
