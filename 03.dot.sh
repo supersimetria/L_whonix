962f7a19527799cfdb70aaef69bec3c9538b9cc5
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-10-01 10:55:59 -0400

    001.005

--- 03.dot.sh
+++ 03.dot.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #titul---------------+
 #
-#ver.001.004
+#ver.001.005
 #
 #--------------------+
 #
 #bash---------------------------------------------------------------------------------------------------------------+
 rm ~/.bashrc*; wget -O ~/.bashrc https://git.io/JuaYS
-#ZSH----------------------------------------------------------------------------------------------------------------+
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-sed -i '11 s/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+#oh-my-zsh----------------------------------------------------------------------------------------------------------+
+(
+ echo n;
+) | sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sed -i '11 s/ZSH_THEME="robbyrussell"/ZSH_THEME="Chicago95"/g' ~/.zshrc
 sed -i '73 s/plugins=(git)/plugins=(npm git npm osx rake sudo dotenv vagrant bundler composer web-search)/g' ~/.zshrc
 git clone https://github.com/powerline/fonts.git
 cd fonts; ./install.sh; cd ..; rm -rf fonts
