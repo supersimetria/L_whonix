23462ca38b97433f3e62d2ff53cfb4bc68e981e4
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-09-30 21:28:23 -0400

    001.004

--- 03.dot.sh
+++ 03.dot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #titul---------------+
 #
-#ver.001.003
+#ver.001.004
 #
 #--------------------+
 #
@@ -13,5 +13,9 @@ sed -i '11 s/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
 sed -i '73 s/plugins=(git)/plugins=(npm git npm osx rake sudo dotenv vagrant bundler composer web-search)/g' ~/.zshrc
 git clone https://github.com/powerline/fonts.git
 cd fonts; ./install.sh; cd ..; rm -rf fonts
+#xfconf-------------------------------------------------------------------------------------------------------------+
+killall xfconfd
+cp ~/L_whonix/mod/01.xfce-perchannel-xml/* ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+xfce4-panel -r
 #link-file----------------------------------------------------------------------------------------------------------+
 #link https://git.io/JuETK
