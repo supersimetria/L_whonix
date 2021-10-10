52978e4403b548b141a352b0671abb77ca76a8d9
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-10-10 13:36:22 -0400

    001.008

--- 03.dot.sh
+++ 03.dot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #titul---------------+
 #
-#ver.001.007
+#ver.001.008
 #
 #--------------------+
 #
@@ -21,3 +21,11 @@ cd fonts; ./install.sh; cd ..; rm -rf fonts
 #xfce4-panel -r
 #link-file----------------------------------------------------------------------------------------------------------+
 #link https://git.io/JuETK
+
+
+#create-------------------------------------------------------------------------------------------------------------+
+cd ~/.config/xfce4/xfconf/xfce-perchannel-xml/; tar -cf suka.tar.gz *.xml; cp suka.tar.gz ~/Desktop/; cd
+
+killall xfconfd
+tar xvzf 1xfconf.tar.gz .config/xfce4/xfconf/xfce-perchannel-xml/
+xfce4-panel -r
